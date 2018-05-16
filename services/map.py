a0d362594828815a380eb738e4f29ec2bf91bf14
cmqmyfox <caroline.martocq@getmyfox.com>
2018-05-16 16:07:50 +0200

    Duration with traffic

--- services/map.py
+++ services/map.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import sys
 import urllib2
+import time
 from   time import sleep
 import json
 import arrow
@@ -10,16 +11,18 @@ class Map():
     UPDATE_INTERVAL_min = 10
 
     def __init__(self):
-        self.url = "https://maps.googleapis.com/maps/api/directions/json?origin=place_id:ChIJuUAJHRy-rhIRFaUq8ZsWn_s&destination=43.558010,1.492678&key=AIzaSyCJUp4B42mhuc1XMWE0fcJJbUGndPyCPMI"
+        self.url_format = 'https://maps.googleapis.com/maps/api/directions/json?origin=place_id:ChIJuUAJHRy-rhIRFaUq8ZsWn_s&destination=43.558010,1.492678&departure_time={}&key=AIzaSyCJUp4B42mhuc1XMWE0fcJJbUGndPyCPMI'
         self.update()
     
     def update(self):
         print ('Updating map...')
         self.last_update = arrow.now()
         try:
-            jsonstr      = urllib2.urlopen(self.url).read()
+            epoch_time   = int(time.time())
+            url          = self.url_format.format(epoch_time)	
+            jsonstr      = urllib2.urlopen(url).read()
             content      = json.loads(jsonstr)
-            duration     = content['routes'][0]['legs'][0]['duration']['text']
+            duration     = content['routes'][0]['legs'][0]['duration_in_traffic']['text']
             self.current = 'Sortie de Labege : {}'.format(duration)			
         except:
             self.current = 'MAP UPDATE ERROR'
